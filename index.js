b645031bfc8720dee28af4f5a6a645cad1684680
Lea Rech <learech@hotmail.com>
2023-02-09 05:35:01 -0300

    subiendo

---
+++ index.js
@@ -0,0 +1,11 @@
+const nombre =  "Leandro"
+
+const apellido = "Rech"
+
+const nombreCompleto = {nombre, apellido}
+
+// sessionStorage.setItem("nombreCompleto", JSON.stringify(nombreCompleto))
+// localStorage.setItem("nombreCompleto", JSON.stringify(nombreCompleto))
+
+const ahora = new Date()
+//document.cookie = `nombreCompleto=${JSON.stringify(nombreCompleto)};expires=${new Date(now.getTime() + 2 * 60000)}`
